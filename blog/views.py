3ce5cd43af02756fc460e78f6611b29f36f771e3
ovmvo <shi_sheng_huo@163.com>
2018-07-31 22:31:44 +0800

    add

--- blog/views.py
+++ blog/views.py
@@ -16,24 +16,24 @@ return_value = {}
 
 
 def index(request):
-    posts_list_datas = Posts.objects.all()
+    posts_list_datas = Posts.objects.filter(status=1).all()
     return_value = get_posts_list_common_data(request, posts_list_datas)
     return render(request, 'index.html', return_value)
 
 
 def article(request, aid):
-    posts_detail_data = get_object_or_404(Posts, id=aid)
-    previous_posts = Posts.objects.filter(publish_time__gt=posts_detail_data.publish_time).last()
-    next_posts = Posts.objects.filter(publish_time__lt=posts_detail_data.publish_time).first()
+        posts_detail_data = get_object_or_404(Posts, id=aid)
+        previous_posts = Posts.objects.filter(publish_time__gt=posts_detail_data.publish_time).last()
+        next_posts = Posts.objects.filter(publish_time__lt=posts_detail_data.publish_time).first()
 
-    return_value['posts_detail_data'] = posts_detail_data
-    return_value['previous_posts'] = previous_posts
-    return_value['next_posts'] = next_posts
-    return render(request, 'article.html', return_value,)
+        return_value['posts_detail_data'] = posts_detail_data
+        return_value['previous_posts'] = previous_posts
+        return_value['next_posts'] = next_posts
+        return render(request, 'article.html', return_value,)
 
 
 def posts_type(request, tid):
-    posts_list_datas = Posts.objects.filter(b_type__id=tid)
+    posts_list_datas = Posts.objects.filter(b_type__id=tid).filter(status=1)
     posts_category = Type.objects.filter(id=tid).first()
     return_value = get_posts_list_common_data(request, posts_list_datas)
     return_value['posts_category'] = posts_category
@@ -41,7 +41,7 @@ def posts_type(request, tid):
 
 
 def posts_time(request, year, month):
-    posts_list_datas = Posts.objects.filter(publish_time__year=year, publish_time__month=month)
+    posts_list_datas = Posts.objects.filter(publish_time__year=year, publish_time__month=month).filter(status=1)
     return_value = get_posts_list_common_data(request, posts_list_datas)
     return_value['posts_time'] = '%s年%s月' % (year, month)
     return render(request, "posts_time.html", return_value, )
