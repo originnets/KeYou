591f183688302045b82eb8af7f155a3293f22379
ovmvo <shi_sheng_huo@163.com>
2018-10-10 15:53:40 +0800

    Update settings.py

--- KeYou/settings.py
+++ KeYou/settings.py
@@ -178,6 +178,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.qq.com'
 EMAIL_PORT = 25
 EMAIL_HOST_USER = '1421863463@qq.com'
-EMAIL_HOST_PASSWORD = 'lnmqcjqsbdiyiabi'  # 授权码
+EMAIL_HOST_PASSWORD = 'lnmqcjqsbdiyiab1i1'  # 授权码
 EMAIL_SUBJECT_PREFIX = '[小火的博客] '
 EMAIL_USE_TLS = True  # 与SMTP服务器通信时，是否启动TLS链接(安全链接)
